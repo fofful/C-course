2f20324aa0d24f3481a676c0c154fd06fc623e49
topi huttunen <topi.huttunen@tuni.fi>
2021-10-02 18:55:04 +0300

    cmd exercise

--- komentorivi.c
+++ komentorivi.c
@@ -3,8 +3,8 @@
 
 int main(int argc, char** argv){
     size_t len_argv = sizeof(argv) / sizeof(argv[1]);
-    for (int i = 0; i < len_argv; i++){
-        
+    for (int i = 1; i < argc; i++){
+        printf("%d: %s (pituus: %d)\n", i, argv[i], strlen(argv[i]));
     }
     
 }
