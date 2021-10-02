bad98db10ccaae94d9b1fb85d403cdba83344676
topi huttunen <topi.huttunen@tuni.fi>
2021-10-02 19:05:09 +0300

    cmd exercise fix

--- komentorivi.c
+++ komentorivi.c
@@ -2,9 +2,9 @@
 #include <string.h>
 
 int main(int argc, char** argv){
-    size_t len_argv = sizeof(argv) / sizeof(argv[1]);
-    for (int i = 1; i < argc; i++){
-        printf("%d: %s (pituus: %d)\n", i, argv[i], strlen(argv[i]));
+    int i;
+    for (i = 1; i < argc; i++){
+        printf("%d: %s (pituus: %u)\n", i, argv[i], (unsigned int)strlen(argv[i]));
     }
-    
+    return 0;
 }
