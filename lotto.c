8cae50c79f882319600b2b966f30bfa77853a1f3
topi huttunen <topi.huttunen@tuni.fi>
2021-10-22 20:27:55 +0300

    final touches to lotto

--- lotto.c
+++ lotto.c
@@ -9,28 +9,20 @@ int main(int argc, char* argv[]){
     int j;
     int corrects;
     corrects = 0;
-    printf("Voittorivi: ");
+    printf("Voittorivi:");
     for(i = 1; i <= 7; i++){
         correctRow[i-1] = atoi(argv[i]);
-        printf("%d", correctRow[i-1]);
-        if (i == 7){
-            printf("\n");
-        }
-        else{
-            printf(" ");
-        }
+        printf(" %d", correctRow[i-1]);
     }
-    printf("Lottorivi: ");
+
+    printf("\n");
+    printf("Lottorivi:");
     for(i = 8; i <= 14; i++){
         guessedRow[i-8] = atoi(argv[i]);
-        printf("%d", guessedRow[i-8]);
-        if (i == 14){
-            printf("\n");
-        }
-        else{
-            printf(" ");
-        }
+        printf(" %d", guessedRow[i-8]);
     }
+
+    printf("\n");
     for(i = 0; i < 7; i++){
         for(j = 0; j < 7; j++){
             if (correctRow[i] == guessedRow[j]){
@@ -38,19 +30,19 @@ int main(int argc, char* argv[]){
             }
         }
     }
-    printf("%d oikein: ", corrects);
-    for(i = 0; i < 7; i++){
-        for(j = 0; j < 7; j++){
-            if (correctRow[j] == guessedRow[i]){
-                printf("%d", correctRow[j]);
-                if (i == 6){
-                    printf("\n");
-                }
-                else{
-                    printf(" ");
+    if (corrects != 0){
+        printf("%d oikein:", corrects);
+        for(i = 0; i < 7; i++){
+            for(j = 0; j < 7; j++){
+                if (correctRow[j] == guessedRow[i]){
+                    printf(" %d", correctRow[j]);
                 }
             }
         }
     }
+    else{
+        printf("Ei yhtään oikein!");
+    }
+    printf("\n");
     return 0;
 }
